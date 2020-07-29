1e4791f57573d9d99f6413fd18d26de877f458b6
Manish Joshi <manishjoshi394@gmail.com>
2020-07-29 12:34:23 +0530

    updated allowed hosts

--- wiki/settings.py
+++ wiki/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['mikipedia.herokuapp.com/']
 
 
 # Application definition
